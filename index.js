df1f05f6dc90f7d09fb689bab18e08a56da3d7b9
bhumi-cka <95154020+bhumi-cka@users.noreply.github.com>
2022-10-13 13:26:34 +0530

    add files

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomDiceImage1="images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src", randomDiceImage1);
+
+
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomDiceImage2="images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src", randomDiceImage2);
+
+
+if (randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩Player 1 Won!";
+}
+else if (randomNumber2>randomNumber1)
+{
+  document.querySelector("h1").innerHTML="Player 2 Won!🚩";
+}
+else if (randomNumber1===randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Draw";
+}
